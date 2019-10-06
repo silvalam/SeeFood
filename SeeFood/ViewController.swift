230f463d8132724c3c02e7e16e87720339861e70
Sylvia Lam <s.silva.lam@gmail.com>
2019-10-06 15:42:44 -0700

    Update UI with hotdog classification result

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -51,7 +51,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 fatalError("Model failed to process image")
             }
             
-            print(results)
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
